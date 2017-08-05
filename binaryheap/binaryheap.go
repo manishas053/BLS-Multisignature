d63b4bc72adba51fb07fea80fae00d4e6fcc70ee
manishas053 <manishas053@gmail.com>
2017-08-05 10:55:44 +0530

    added a new function to insert an element to the existing binary heap

--- binaryheap/binaryheap.go
+++ binaryheap/binaryheap.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "os"
+)
 
 
 func heapify(i int, heap []int) []int {
@@ -23,20 +26,56 @@ func swap(num1, num2 int) (int, int) {
 }
 
 func main() {
-  var len, num, option int
+  var len int
+  fmt.Println("Enter the maximum length : ")
+  fmt.Scanln(&len)
+  heap := make([]int, len+1)
+  for {
+    fmt.Println("Want to continue (y or n) ?")
+    option(heap)
+  }
+}
+
+func option(heap []int) {
+  var char string
+  fmt.Scanln(&char)
+  switch char {
+  case "y":
+    sample(heap)
+    return
+  case "n":
+    os.Exit(2)
+  default:
+    fmt.Println("Invalid input : Press y to continue or n to exit ")
+  }
+}
+
+func sample(heap []int) {
+  var limit, num, option int
   fmt.Printf("Choose any option\n1. Create a heap\n2. Insert a node\n3. Delete a node\n")
   fmt.Scanln(&option)
   switch option {
   case 1:
-    fmt.Println("Enter the length : ")
-    fmt.Scanln(&len)
-    heap := make([]int, len+1)
+    fmt.Println("Enter the number of elements : ")
+    fmt.Scanln(&limit)
     fmt.Println("Enter elements : ")
-    for i := 1; i <= len; i ++ {
+    for i := 1; i <= limit; i ++ {
+      fmt.Scanln(&num)
+      heap[i] = num
+      heap = heapify(i, heap)
+    }
+    fmt.Println(heap)
+    return
+
+  case 2:
+    fmt.Println("Enter the element to be inserted : ")
     fmt.Scanln(&num)
+    i := 1
+    for heap[i] != 0 {
+      i ++
+    }
     heap[i] = num
     heap = heapify(i, heap)
-}
-fmt.Println(heap)
+    fmt.Println(heap)
   }
 }
